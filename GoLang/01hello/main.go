9b5fbb01d6fb4628a5d9505784c3e9c1c3f07ad8
Shivam Singh <85937631+chromazo@users.noreply.github.com>
2024-05-26 19:49:43 +0530

    Add files via upload

---
+++ GoLang/01hello/main.go
@@ -0,0 +1,15 @@
+// go mod init hello
+// install the GO extension in VS code
+
+package main
+
+import "fmt" //this was automatically imported when the below code was saved no need to manually write import
+
+func main() {
+	fmt.Println("hello from John") //this will print "--" in the terminal
+}
+
+// type go run <file name> to run file in the terminal
+
+//go help or go help <command name> to know more about go and its commands here are the docs
+// go path GOPATH - to get the path where go is installed in local system
