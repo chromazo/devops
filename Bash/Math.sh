78b42181605ab2c5572688947e184cb634726b02
Shivam Singh <85937631+chromazo@users.noreply.github.com>
2024-05-30 17:45:53 +0530

    Create Math.sh

---
+++ Bash/Math.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+#Math functions
+# expr --> evaluate expression
+# echo $SHELL --> tells whihch shell is being used
+
+$a=10
+$b=5
+echo "Value of a: $a, b:$b"
+expr $a + $b
+expr $a - $b
+expr $a / $b
+expr $a \* $b
