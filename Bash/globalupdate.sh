a2805cd65056099582a1b500fdfc7bb8ee1642f3
chromazo <chromazo@gmail.com>
2024-05-31 19:15:10 +0000

    using functions

--- Bash/globalupdate.sh
+++ Bash/globalupdate.sh
@@ -4,18 +4,22 @@ release=/etc/os-release
 updater=/var/log/updater.log
 error=/var/log/updater_error.log
 
+chech_exit_status() {
+    if [$? -ne 0 ]
+    then
+         echo "We encountered some error while updating the system. please try again"
+    else
+         echo "system updated successfully"
+    fi
+}
+
 if grep -q "Arch" $release
 then
         #The host is based on Arch, run the pacman update command
         echo "===================================================" >> $updater
         date >> $updater
         sudo packman -Syu 1>> $updater 2>> $error
-	if [ $? -ne 0 ]
-	then
-		echo "We encountered some error while updating the system. please try again"
-	else
-		echo "system updated successfully"
-	fi
+	chech_exit_status
 fi
 
 if grep -q "Ubuntu" $release || grep -q "Debian" $release
@@ -25,10 +29,5 @@ then
         echo "===================================================" >> $updater
         date >> $updater
         sudo apt update -y && sudo apt dist-upgrade -y  1>> $updater 2>> $error
-	 if [ $? -ne 0 ]
-        then
-                echo "We encountered some error while updating the system. please try again"
-        else
-                echo "system updated successfully"
-        fi
+        chech_exit_status
 fi
