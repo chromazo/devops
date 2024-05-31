58fac86358da81168836fcc555cb0281a93301df
Shivam Singh <85937631+chromazo@users.noreply.github.com>
2024-05-31 14:54:08 +0530

    Update exitcodes.sh

--- Bash/exitcodes.sh
+++ Bash/exitcodes.sh
@@ -6,7 +6,8 @@ if command -v $package
 then
         echo "The $package package is already insatalled in the below path"
         which $package
-        exit 0 # this command will make to exit the script with exit value 0, other codes after this will not be >else
+        exit 0 # this command will make to exit the script with exit value 0, other codes after this will not be
+else
         echo "$package package is not available. proceeding to install..."
         echo "checking system... starting system update process"
         sudo apt update -y >> /system_update.log && sudo apt upgrade -y >> /system_update.log
