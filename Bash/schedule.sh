bf1c904b57c8568a7868c893ced3f4a0ba2f4065
chromazo <github.accNew@gmail.com>
2024-06-02 11:38:20 +0000

    update schedule.sh

--- Bash/schedule.sh
+++ Bash/schedule.sh
@@ -4,3 +4,7 @@ jobfile=/jobFileLogs.log
 
 echo "This job ran at $(date)" >> $jobfile
 
+
+
+# we can run "atq" command to check the jobs that are scheduled. (It gives job id, time of execurion and owner)
+# "atrm" to can a job, we have to provide job id. eg --> atrm 3
