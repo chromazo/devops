b72d247a4cabe67e3d7cb0dbc2a3f6f04bf52046
Shivam Singh <github.accNew@gmail.com>
2024-06-07 00:57:47 +0530

    Update main.go

--- GoLang/07mypointer/main.go
+++ GoLang/07mypointer/main.go
@@ -6,7 +6,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome!!")
-	var ptr *int // we define a variable as a pointer by giving "*"<variable type> . eg: *int
+	// var ptr *int // we define a variable as a pointer by giving "*"<variable type> . eg: *int, *string
 
-	fmt.Println("The value of pointer is: ", ptr) // Default value is <nill>
+	// fmt.Println("The value of pointer is: ", ptr) // Default value is <nill>
+
+	num := 100
+	var ptr = &num // "&" is used by pointer for referencing a variable
+
+	fmt.Println("The value of actual pointer is ", ptr)  //this will print the address of the variable where pointer is pointing
+	fmt.Println("The value of actual pointer is ", *ptr) //print the value inside the pointer reference
 }
