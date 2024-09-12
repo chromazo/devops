6d72e833e18e22176530b4b3f80253aef821eb30
Shivam Singh <github.accNew@gmail.com>
2024-09-12 17:16:26 +0530

    Create variable.cs

---
+++ C#/variable.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            string Name = Console.ReadLine(); //Takes input from user and saves it in variable "Name"
+            Console.WriteLine("Hello, my name is "+Name);
+            Console.ReadLine();
+
+            /*
+             * there are many datatypes here, like
+             * int
+             * string
+             * booolean
+             * float,etc 
+             */
+        }
+    }
+}
